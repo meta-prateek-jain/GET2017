b7e180ab4ca964427e4023e68b0d601230f32950
meta-prateek-jain <prateek.jain@metacube.com>
2017-08-29 23:08:20 +0530

    DataStructure Assignment 6

--- DataStructure-Assignment6/com/metacube/ds6/employee/test/TestEmployeeSorting.java
+++ DataStructure-Assignment6/com/metacube/ds6/employee/test/TestEmployeeSorting.java
@@ -27,29 +27,23 @@ public class TestEmployeeSorting {
 	@Before
 	public void setUp() {
 		collections = new EmployeeCollection();
-		employeesList = collections.getArrayList();
-		employeeSet = collections.getSet();
 	}
 	/**
 	 * Method is used to test the employees entry sorted in natural order or not
 	 */
 	@Test
 	public void testEmployeesSortingInNaturalOrder() {
+		employeesList = collections.getArrayList();
 		employeesList.add(new Employee(23, "mahesh", "kumbhamarg"));
 		employeesList.add(new Employee(20, "manish", "sitapura"));
 		employeesList.add(new Employee(15, "mukesh", "haldighati"));
 		employeesList.add(new Employee(30, "ramesh", "durgapura"));
 		collections.sort(employeesList);
-		String actual = "";
-		// loop continue till end of employees list is reached
-		for (Employee employee : employeesList) {
-			actual += "Id: " + employee.getId()+ " Name: " + employee.getName() + " address: "+ employee.getAddress()+"\n";
-		}
-		String expected = "Id: 15 Name: mukesh address: haldighati"
-				+ "\nId: 20 Name: manish address: sitapura"
-				+ "\nId: 23 Name: mahesh address: kumbhamarg"
-				+ "\nId: 30 Name: ramesh address: durgapura\n";
-		assertEquals(expected, actual);
+		String expected = "[\nId: 15 --> Name: mukesh --> Address: haldighati, "
+				+ "\nId: 20 --> Name: manish --> Address: sitapura, "
+				+ "\nId: 23 --> Name: mahesh --> Address: kumbhamarg, "
+				+ "\nId: 30 --> Name: ramesh --> Address: durgapura]";
+		assertEquals(expected, employeesList.toString());
 	}
 
 	/**
@@ -57,21 +51,17 @@ public class TestEmployeeSorting {
 	 */
 	@Test
 	public void testEmployeesSortingByName(){
+		employeesList = collections.getArrayList();
 		employeesList.add(new Employee(23, "mahesh", "kumbhamarg"));
 		employeesList.add(new Employee(20, "manish", "sitapura"));
 		employeesList.add(new Employee(15, "mukesh", "haldighati"));
 		employeesList.add(new Employee(30, "ramesh", "durgapura"));
 		collections.sort(employeesList, Employee.NameComparator);
-		String actual = "";
-		// loop continue till end of employees list is reached
-		for (Employee employee : employeesList) {
-			actual += "Id: " + employee.getId()+ " Name: " + employee.getName() + " address: "+ employee.getAddress()+"\n";
-		}
-		String expected = "Id: 23 Name: mahesh address: kumbhamarg"
-				+ "\nId: 20 Name: manish address: sitapura"
-				+ "\nId: 15 Name: mukesh address: haldighati"
-				+ "\nId: 30 Name: ramesh address: durgapura\n";
-		assertEquals(expected, actual);
+		String expected = "[\nId: 23 --> Name: mahesh --> Address: kumbhamarg, "
+				+ "\nId: 20 --> Name: manish --> Address: sitapura, "
+				+ "\nId: 15 --> Name: mukesh --> Address: haldighati, "
+				+ "\nId: 30 --> Name: ramesh --> Address: durgapura]";
+		assertEquals(expected, employeesList.toString());
 	}
 	
 	/**
@@ -79,14 +69,14 @@ public class TestEmployeeSorting {
 	 */
 	@Test
 	public void testEmployeesEntryUniqueOrNot(){
+		employeeSet = collections.getSet();
 		employeeSet.add(new Employee(23, "mahesh", "kumbhamarg"));
 		employeeSet.add(new Employee(15, "mukesh", "haldighati"));
 		employeeSet.add(new Employee(23, "mahesh", "kumbhamarg"));
 		employeeSet.add(new Employee(30, "ramesh", "durgapura"));
-		String actual = employeeSet.toString();
 		String expected = "[\nId: 23 --> Name: mahesh --> Address: kumbhamarg, "
 				+ "\nId: 30 --> Name: ramesh --> Address: durgapura, "
 				+ "\nId: 15 --> Name: mukesh --> Address: haldighati]";
-		assertEquals(expected, actual);
+		assertEquals(expected, employeeSet.toString());
 	}
 }
